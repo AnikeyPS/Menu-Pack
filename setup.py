6b26a2e421e6667ddc84c470b0bc7e10d0d05610
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-09-26 13:23:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from distutils.core import setup
-setup(name='Menu', version='0.2.0', packages=['Menu-Pack'], package_dir={'Menu': 'Menu-Pack'})
+setup(name='Menu', version='0.2.0', packages=['Menu-Pack'], package_dir={'Menu': 'Menu-Pack', 'Menu.files': 'Menu-Pack/files'})
