c492c886b27509230c09e54e05703af407a71994
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-09-27 13:34:15 +0300

    Update 1.py

--- examples/1.py
+++ examples/1.py
@@ -1,8 +1,5 @@
 from menu.core import MenuApp
-app = MenuApp(['README.md', '''print('d')
-def a():
-    print('aaa')
-a()'''], ['Test setup', 'link://setup.test'])
+app = MenuApp(['Hello', 'Hello) ;-}'], ['Test setup', 'link://setup.test'])
 app.title('Example window')
 app.init()
 app.loop()
