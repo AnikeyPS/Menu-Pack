7102fb9d9b5e94b538b0da72f0af9fd33b0f6365
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-09-27 13:25:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from distutils.core import setup
-setup(name='menu', version='0.2.0', packages=['menu', 'menu.files'], package_dir={'menu': 'menu',
-                                                                                  'menu.files': 'menu\\files'})
+
+setup(name='menu', version='0.2.0',
+      packages=['menu', 'menu.files', 'menu_examples'],
+      package_dir={'menu': 'menu', 'menu.files': 'menu\\files', 'menu_examples': 'examples'})
