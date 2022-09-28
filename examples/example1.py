4bb950353af723b5ab1dcd3cf9f81500832f59aa
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-09-28 09:46:46 +0300

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -1,7 +1,7 @@
 from menu.core import MenuApp
-app = MenuApp(['Click', 'command://print(\'Hello!\')'], ['About', 'This is a example code'])
-app.title('Example')
-app = MenuApp(['Hello', 'Hello) ;-}'], ['Test setup', 'link://setup.test'])
-app.title('Example window')
-app.init()
-app.loop()
+example = MenuApp(['Click', 'command://print(\'Hello!\')'], ['About', 'This is a example code'])
+example.title('Example')
+example = MenuApp(['Hello', 'Hello) ;-}'], ['Test setup', 'link://setup.test'])
+example.title('Example window')
+example.init()
+example.loop()
