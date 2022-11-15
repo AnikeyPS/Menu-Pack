b72e8ae17390656893363eb1d36d29105bed80c1
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-11-15 11:40:02 +0300

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -2,4 +2,3 @@ from menu_pack.core import MenuApp
 example = MenuApp(['Click', 'command://print(\'Hello!\')'], ['About', 'This is a example code'])
 example.title('Example window')
 example.init()
-example.loop()
