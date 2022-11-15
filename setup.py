550112b2bc7da59e2f3ae8659d12c2e39b94b5ef
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-11-15 11:36:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from menu import __version__
 
 setup(name='menu', version=__version__,
       packages=['menu', 'menu.files', 'menu_examples'],
-      package_dir={'menu': 'menu', 'menu.files': 'menu\\files', 'menu_examples': 'examples'})
+      package_dir={'menu': 'menu_pack', 'menu_pack.files': 'menu\\files', 'menu_pack.examples': 'examples'})
