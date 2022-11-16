45b3e458929a22abb066eb39a3d2f1d54e0b74e1
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-11-16 09:42:05 +0300

    Update example2.py

--- examples/example2.py
+++ examples/example2.py
@@ -1,1 +1,13 @@
+import threading as th
+from menu_pack.core import MenuApp
 
+
+def _run():
+    app = MenuApp(["Button 1": "Read button", "Pause": "command://__import__('os').system('pause')"])
+    app.init()
+    app.loop()
+
+
+obj = th.Thread(target=_run)
+obj.start()
+obj.join()
