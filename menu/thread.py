b0c1b49d73b70d683aa770cc25bbe53a2918dead
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-11-16 09:37:31 +0300

    Delete thread.py

--- menu/thread.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-def thread_loop(obj):
-    def _run(app):
-        copy_app = app
-        copy_app.loop()
-    thread_obj = threading.Thread(target=lambda: _run(obj))
-    thread_obj.start()
-    thread_obj.join()
