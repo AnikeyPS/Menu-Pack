1fcf2e13160a286fe49f0b39c8b5cbc239272144
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-11-16 09:29:10 +0300

    Update thread.py

--- menu/thread.py
+++ menu/thread.py
@@ -1,4 +1,9 @@
 import threading
 
-def thread_loop():
-
+def thread_loop(obj):
+    def _run(app):
+        copy_app = app
+        copy_app.loop()
+    thread_obj = threading.Thread(traget=_run, args=(obj))
+    thread_obj.start()
+    thread_obj.join()
